368dd2d9dce7d58cc41dc026051c8527ccd18cfd
Pratyaksh Pandey <232079021+24f2006816@users.noreply.github.com>
2026-07-01 14:40:40 +0530

    Time: 0 ms (100%), Space: 24.4 MB (40.53%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int cnt = 0;
+    int ans = -1;
+
+    void inorder(TreeNode* root, int k) {
+        if (root == nullptr || ans != -1)
+            return;
+
+        inorder(root->left, k);
+
+        cnt++;
+
+        if (cnt == k) {
+            ans = root->val;
+            return;
+        }
+
+        inorder(root->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root, k);
+        return ans;
+    }
+};
