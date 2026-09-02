4cbf5c352abc11e88c950918378f2031a01780a1
Pratyaksh Pandey <232079021+24f2006816@users.noreply.github.com>
2026-09-02 08:45:04 +0530

    Time: 3 ms (31.01%) | Memory: 11.7 MB (75.35%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> result;
+        vector<string> v;
         
         for (int i = 1; i <= n; i++) {
             if (i % 15 == 0) {
-                result.push_back("FizzBuzz");
+                v.push_back("FizzBuzz");
             }
             else if (i % 3 == 0) {
-                result.push_back("Fizz");
+                v.push_back("Fizz");
             }
             else if (i % 5 == 0) {
-                result.push_back("Buzz");
+                v.push_back("Buzz");
             }
             else {
-                result.push_back(to_string(i));
+                v.push_back(to_string(i));
             }
         }
-        return result;
+        return v;
     }
 };
