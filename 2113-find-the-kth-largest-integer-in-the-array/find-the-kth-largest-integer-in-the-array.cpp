26d6c5321852070f48307a9819348225f5cde150
Pratyaksh Pandey <232079021+24f2006816@users.noreply.github.com>
2026-09-05 20:09:23 +0530

    Time: 499 ms (9.78%) | Memory: 341.6 MB (7.03%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end(), [](string a, string b){
             if (a.length() != b.length())
-                return a.length() < b.length();
-            return a < b;
+                return a.length() > b.length();
+            return a > b;
         });
-        return nums[nums.size()-k];
+        return nums[k-1];
     }
 };
