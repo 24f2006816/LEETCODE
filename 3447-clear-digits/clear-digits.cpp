a921abe81f36cf5b8d5bfe764c9eb56b0ecdef47
Pratyaksh Pandey <232079021+24f2006816@users.noreply.github.com>
2026-09-09 20:53:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (83.22%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string st;
+        for (char c : s){
+            if (c >= '0' && c <= '9'){
+                st.pop_back();
+            }
+            else{
+                st.push_back(c);
+            }
+        }
+        return st;
+    }
+};
