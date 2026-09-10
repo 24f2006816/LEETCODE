dd77916be106db74bdc6508fc52c0827063a6692
Pratyaksh Pandey <232079021+24f2006816@users.noreply.github.com>
2026-09-11 03:35:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.06%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = 0;
+        int temp = num;
+        while (temp > 0){
+            mask = mask*2 + 1;
+            temp /= 2;
+        }
+        return num ^ mask;
+    }
+};
